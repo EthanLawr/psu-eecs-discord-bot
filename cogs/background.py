d9d9cfdccba1afdd55a2bedd5fee01a2bffea5d5
unknown <gerardmartinp@gmail.com>
2024-12-19 11:03:55 -0600

    Updates to bot/functions with 2.0 migration

--- cogs/background.py
+++ cogs/background.py
@@ -84,7 +84,8 @@ class Background(commands.Cog):
 		Helper fuction to reorder the class channels by thier respective member counts
 	"""
 	async def reorder_channels(self):
-		BLACKLIST = ["Admin","GiveawayBot","Bots","Mod","dabBot","@everyone","Simple Poll","Groovy"]
+		BLACKLIST = ["Admin","GiveawayBot","Bots","Mod","dabBot","Simple Poll","Groovy"]
+		# @everyone is position 0
 
 		psu_discord = self.bot.get_guild(575004997327126551)
 		raw_roles = psu_discord.roles
